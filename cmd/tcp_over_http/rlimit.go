87de8511c1b06704fae657386530276d9e321fe5
David Dusty <40916787+daviddusty@users.noreply.github.com>
2023-06-15 03:52:56 +0100

    Update rlimit.go

--- cmd/tcp_over_http/rlimit.go
+++ cmd/tcp_over_http/rlimit.go
@@ -6,19 +6,20 @@ import (
 )
 
 func init() {
+	log.Printf("test")
 	// increaseRlimitNofile()
 }
 
-func increaseRlimitNofile() {
-	// var lim syscall.Rlimit
+// func increaseRlimitNofile() {
+// 	// var lim syscall.Rlimit
 
-	// if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &lim); err != nil {
-	// 	log.Printf("unable to get current RLIMIT_NOFILE: %v", err)
-	// 	return
-	// }
-	// lim.Cur = lim.Max
-	// if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &lim); err != nil {
-	// 	log.Printf("unable to set new RLIMIT_NOFILE: %v", err)
-	// 	return
-	// }
-}
+// 	// if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &lim); err != nil {
+// 	// 	log.Printf("unable to get current RLIMIT_NOFILE: %v", err)
+// 	// 	return
+// 	// }
+// 	// lim.Cur = lim.Max
+// 	// if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &lim); err != nil {
+// 	// 	log.Printf("unable to set new RLIMIT_NOFILE: %v", err)
+// 	// 	return
+// 	// }
+// }
